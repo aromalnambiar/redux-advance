129a1bf8013abb047c6960d4114598a50b5ec65e
aromalnambiar <aromalndr@gmail.com>
2023-11-20 07:03:52 +0530

    creating asynchronus redux functionality

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+import axios from 'axios';
+import BASE_URL from './BASE_URL';
 
 function App() {
 
   const showCart = useSelector(state => state.show.showCart)
+  const cart = useSelector(state => state.cart)
+
+  const sentData =  async () => {
+    try {
+      axios.put(`${BASE_URL}/cart.json`, JSON.stringify(cart))
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+    sentData()
+  }, [cart])
 
   return (
     <Layout>
---
+++ src/BASE_URL.js
@@ -0,0 +1,3 @@
+const BASE_URL = "https://redux-cart-836d7-default-rtdb.firebaseio.com/"
+
+export default BASE_URL;
